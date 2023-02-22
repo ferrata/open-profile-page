6020da829dfd6259dee0d08799d1c55ea71118c3
Tim Goloshchapov <tim.goloschapov@gmail.com>
2023-02-22 16:38:34 -0600

    api data rendering

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,14 +14,13 @@ export default function Home() {
   //   name?: string
   // }
 
-  const [profile, setProfile] = useState<any>('')  
+  const [profile, setProfile] = useState<any>({})
 
   useEffect(()=>{
     axios.get('https://ferrata.builtwithdark.com/open-profile/api/v1/profile/timbobeek')   //gotta make the username part dynamic `${username}`
       .then(res=>{
-        //console.log('res.data', res.data);
-        setProfile(res.data.gitHub.profile); // this way it shows up but with delay  
-        //setProfile(res.data.gitHub.profile); // this causes an error
+        console.log('res.data', res.data);
+        setProfile(res.data);  
         //console.log('profile', profile);
       })
       .catch(err =>{
@@ -30,7 +29,7 @@ export default function Home() {
     
   },[]);
 
-  const {name} = profile;
+  //const {name} = profile;
 
   return (
     <>
@@ -43,7 +42,8 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.center}>
           <div className={styles.description}>
-            {name}
+            {profile?.gitHub?.profile?.avatarUrl}<br/>
+            {profile?.gitHub ? profile?.gitHub?.profile?.name :  "loading..."}
           </div>
         </div>
       </main>
